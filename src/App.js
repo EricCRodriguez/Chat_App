d49bdba52ba6b5408ba95fcc4f626a790b5c9996
EricCRodriguez <ecrod12@gmail.com>
2021-02-11 15:51:18 -0600

    setup LoginForm-set up build folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import { ChatEngine } from 'react-chat-engine';
-import './App.css';
-import ChatFeed from './components/ChatFeed.jsx';
+import { ChatEngine } from "react-chat-engine";
+
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "ee28931c-829a-4bd1-99c9-ce1a0a8a91ab";
 
 const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="ee28931c-829a-4bd1-99c9-ce1a0a8a91ab"
-      userName="secondcitydev"
-      userSecret="RAy5Xi4knXhnEHV"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
     />
-  );   
+  );
 };
 
-  
+// infinite scroll, logout, more customizations...
 
 export default App;
--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,6 +8,21 @@ const ChatFeed = (props) => {
 
   const chat = chats && chats[activeChat];
 
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isMyMessage ? "right" : "left",
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
+            }}
+          />
+        )
+    );
   const renderMessages = () => {
     const keys = Object.keys(messages);
 
@@ -34,7 +49,9 @@ const ChatFeed = (props) => {
               marginRight: isMyMessage ? "18px" : "0px",
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
-          ></div>
+          >
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
         </div>
       );
     });
